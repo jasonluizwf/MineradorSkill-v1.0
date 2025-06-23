e84a9561e18d2e12040b1d26b66aea41116f2d05
victor <victorluizfacul@gmail.cm>
2025-06-23 13:13:50 -0300

    Iniciado objetos para usar em todo o funcionamento singleton

--- src/main/java/com/victor/minerador/Minerador.java
+++ src/main/java/com/victor/minerador/Minerador.java
@@ -5,12 +5,9 @@ import com.victor.minerador.data.PlayerManager;
 import com.victor.minerador.listeners.MinerarListener;
 import com.victor.minerador.utils.BarraDeProgressoUtil;
 import com.victor.minerador.utils.ItemFactory;
-import com.victor.minerador.utils.PremiacaoPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.Objects;
-
 
 public final class Minerador extends JavaPlugin {
 
@@ -20,17 +17,14 @@ public final class Minerador extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
-        this.playerManager = new PlayerManager(this);
-        this.itemFactory = new ItemFactory(this);
-        PremiacaoPlayer premiacao = new PremiacaoPlayer(this);
+        itemFactory = new ItemFactory(this);
+        playerManager = new PlayerManager(this);
 
         // Plugin startup logic
-        Bukkit.getPluginManager().registerEvents(new MinerarListener(this, premiacao), this);
+        Bukkit.getPluginManager().registerEvents(new MinerarListener(this, itemFactory), this);
         Objects.requireNonNull(getCommand("minerador")).setExecutor(
                 new ComandoMinerador(playerManager, barraDeProgressoUtil)
         );
-
         getLogger().info("Plugin Minerador ativado!");
     }
 
@@ -39,5 +33,4 @@ public final class Minerador extends JavaPlugin {
         // Plugin shutdown logic
     }
     public PlayerManager getPlayerManager() { return playerManager; }
-    public ItemFactory getItemFactory() { return itemFactory; }
 }
